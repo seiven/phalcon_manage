5d7e0285aa57e3c2230ba55cc91988236cfd09c8
seiven <c.navy@qq.com>
2015-12-03 11:35:36 +0800

    modules bug,menu bug

---
+++ bootstrap.php
@@ -0,0 +1,51 @@
+<?php
+use Phalcon\Mvc\Application;
+use Phalcon\Config\Adapter\Ini as ConfigIni;
+use Phalcon\DI\FactoryDefault;
+use Phalcon\Mvc\Dispatcher;
+/**
+ * Read the configuration
+ */
+$config = new ConfigIni(__DIR__ . '/config/config.ini');
+if(is_readable(__DIR__ . '/config/config.dev.ini')){
+	$override = new ConfigIni(__DIR__ . '/config/config.dev.ini');
+	$config->merge($override);
+}
+
+/**
+ * The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
+ */
+$di = new FactoryDefault();
+
+/**
+ * We register the events manager
+ */
+$di->set('dispatcher', function () use($di){
+	$dispatcher = new Dispatcher();
+	return $dispatcher;
+});
+/**
+ * Auto-loader configuration
+ */
+require __DIR__ . '/config/loader.php';
+
+/**
+ * Load application services
+ */
+require __DIR__ . '/config/services.php';
+
+$application = new Application($di);
+
+// register moudles
+$modules = require __DIR__ . '/config/modules.php';
+foreach($modules as $m){
+	$_modules[$m] = array(
+			'className'=> "{$m}\Module",
+			'path'=> __DIR__ . "/{$m}/Module.php"
+	);
+}
+$application->registerModules($_modules);
+// router
+$di->set('router', function () use($application){
+	return require __DIR__ . '/config/routes.php';
+});
